b6e60bc84513b8a7d099538d2af3e071dae2cd26
Jeanneret Loïck <lock.jeanneret@gmail.com>
2015-12-08 15:59:24 +0100

    Login page general design finished. Basic javascript linking script created. File structure altered.
    Work done by Blatoy, Michael Ligotino & Victor Feller.

--- client/resources/logic/core/init.js
+++ client/resources/logic/core/init.js
@@ -1,1 +1,2 @@
-$("#content").load( "../../../pages/login.html" );
+$("head").append('<link rel="stylesheet" href="resources/ui/styles/login.css" type="text/css" />');
+$("#content").load( "pages/login.html" );
