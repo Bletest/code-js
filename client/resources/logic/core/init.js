80e5d6898711c318df8400381cdaa6bae6266a7f
Jeanneret Loïck <lock.jeanneret@gmail.com>
2016-01-05 16:54:26 +0100

    Project manager base design

--- client/resources/logic/core/init.js
+++ client/resources/logic/core/init.js
@@ -1,2 +1,3 @@
-$("head").append('<link rel="stylesheet" href="resources/ui/styles/login.css" type="text/css" />');
-$("#content").load( "pages/login.html" );
+$("head").append('<link rel="stylesheet" href="resources/ui/styles/project-manager.css" type="text/css" />');
+$("#top-bar").load( "pages/project-manager.html #page-topbar" );
+$("#content").load( "pages/project-manager.html #page-content" );
