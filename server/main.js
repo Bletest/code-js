3519f455f91766c34f1157138f34780ae2202407
MichaelLigotino <mic.ligotino@gmail.com>
2016-02-16 14:58:00 +0100

    New version with ws listening and classes added in the main.

--- server/main.js
+++ server/main.js
@@ -4,37 +4,47 @@
 // Global variables
 var modules = {core:{}, classes:{}, config:{}};
 var controller;
+var socket;
+
+// INITIALISATION
 
 function init() {
+	// Include all files
+	includeLogic();
+	
+	// Create controller
+	controller = new modules.classes.Controller(new modules.classes.User(), new modules.classes.File(), new modules.classes.Project());
+	controller.test();
+	
+	// Create socket
+	socket = new modules.classes.Socket();
+	socket.init();
+	
+	// Websocket
+	socket.ws.on("connection", function() {
+		console.log("Connection occured");
+	});
+}
+
+function includeLogic() {
 	// Link core files
+	modules.core.Editor = require("./core/editor.js");
+	modules.core.Login = require("./core/login.js");
+	modules.core.Project = require("./core/project.js");
 	
 	// Link class files
 	modules.classes.Controller = require("./class/Controller.js");
 	modules.classes.User = require("./class/User.js");
 	modules.classes.File = require("./class/File.js");
 	modules.classes.Project = require("./class/Project.js");
+	modules.classes.Client = require("./class/Client.js");
+	modules.classes.Database = require("./class/Database.js");
+	modules.classes.Message = require("./class/Message.js");
+	modules.classes.Socket = require("./class/Socket.js");
 	
 	// Link config
-	
-	// Link
-	
-	// Create controller
-	controller = new modules.classes.Controller(new modules.classes.User(), new modules.classes.File(), new modules.classes.Project());
-	controller.test();
-	// Create class object
-	
-	// Load config
-	// resources/config/
-	
-	// Create socket
-	
-	// includeLogic();
-	// Create global objects 
-}
-
-function includeLogic() {
-	// Call core
-	// Call all classes
+	modules.config.Database = require("./resources/config/database.js");
+	modules.config.Global = require("./resources/config/global.js");
 }
 
 // Switch -> redirect core/xx
