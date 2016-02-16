8f445896a33a72ae4a14c9fe0da54701444e9bf5
Jeanneret Loïck <lock.jeanneret@gmail.com>
2016-02-16 14:56:45 +0100

    Global config v1 finished Database config finished

---
+++ server/resources/config/database.js
@@ -0,0 +1,53 @@
+var exports = {
+	path: "./data/database/CodeDB.db",
+	tables: {
+		project: {
+			name: "project",
+			fields: {
+				id: "ProjectID",
+				name: "Name",
+				isFolder: "CreationDate",
+				isDeleted: "DeletionDate",
+				creationDate: "CreationUserId",
+				lastEditionDate: "DeletionUserId",
+				deletionDate: "Description"				
+			}
+		},
+		file: {
+			name: "file",
+			fields: {
+				id: "FileID",
+				name: "Name",
+				isFolder: "IsFolder",
+				isDeleted: "IsDeleted",
+				creationDate: "CreationDate",
+				lastEditionDate: "LastEditionDate",
+				deletionDate: "DeletionDate",
+				parentFolderId: "ParentFolderId",
+				projectId: "ProjectId",
+				creationUserId: "CreationUserId",
+				editionUserId: "EditionUserId",
+				deletionUserId: "DeletionUserId"				
+			}
+		},
+		relUserProject: {
+			name: "rel_user_project",
+			fields: {
+				userId: "UserID",
+				projectId: "ProjectID",
+				permissionLevel: "PermissionLevel",
+				userColor: "UserColor"
+			}
+		},
+		user: {
+			name: "user",
+			fields: {
+				userId: "UserID",
+				username: "Username",
+				pass: "Password",
+				avatarURL: "AvatarURL",
+				inscriptionDate: "InscriptionDate"
+			}
+		}
+	}
+}
---
+++ server/resources/config/global.js
@@ -0,0 +1,6 @@
+var exports = {
+	websocket: {
+		port: 49111
+	},
+	projectFolder: "./data/files"
+}
