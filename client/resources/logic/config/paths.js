53d6cab9bd401c5341780224306fa74f3d1dd74a
Jeanneret Loïck <lock.jeanneret@gmail.com>
2016-02-23 13:38:48 +0100

    Created paths and updated main.js

---
+++ client/resources/logic/config/paths.js
@@ -0,0 +1,12 @@
+var GLOBAL_PATHS = {};
+
+GLOBAL_PATHS["root"] = "/";
+GLOBAL_PATHS["resources"]		= GLOBAL_PATHS["root"] 		+ "resources/";
+GLOBAL_PATHS["pages"] 			= GLOBAL_PATHS["root"] 		+ "pages/";
+GLOBAL_PATHS["ui"] 				= GLOBAL_PATHS["resources"] + "ui/";
+GLOBAL_PATHS["logic"] 			= GLOBAL_PATHS["resources"] + "logic/";
+GLOBAL_PATHS["core"] 			= GLOBAL_PATHS["logic"] 	+ "core/";
+GLOBAL_PATHS["config"]			= GLOBAL_PATHS["logic"] 	+ "config/";
+GLOBAL_PATHS["tools"] 			= GLOBAL_PATHS["logic"]		+ "tools/";
+GLOBAL_PATHS["images"] 			= GLOBAL_PATHS["ui"] 		+ "images/";
+GLOBAL_PATHS["styles"] 			= GLOBAL_PATHS["ui"]		+ "styles/";
--- client/resources/logic/main.js
+++ client/resources/logic/main.js
@@ -1,3 +1,14 @@
+/*
 $("head").append('<link rel="stylesheet" href="resources/ui/styles/editor.css" type="text/css" />');
 $("#top-bar").load( "pages/editor.html #page-topbar" );
-$("#content").load( "pages/editor.html #page-content" );
+$("#content").load( "pages/editor.html #page-content" );*/
+
+
+// Load #top-bar and #content from filename
+// Replace #copyright by the copyright text
+function changePage(fileName) {
+	$("#top-bar").html("");
+	$("#content").html("");
+	$("#top-bar").load( + " #page-header" );
+	$("#top-bar").load( + " #page-content" );
+}
