2bd21eb4df6fe1e8769e4366b5dce81e79b5776b
MichaelLigotino <mic.ligotino@gmail.com>
2016-03-05 15:32:01 +0100

    Style of the project-manager page has been improved. Since I had problems with the changePage function I added some variables in the config files in order to actually load the pages and be able to work.

--- client/resources/logic/main.js
+++ client/resources/logic/main.js
@@ -1,10 +1,14 @@
-
 var login, editor, manager;
+
+/*
+Problems with the paths, I had to change them into relatives paths. Michael
+*/
 
 // Include the paths config
 $.getScript("resources/logic/config/paths.js", function() {
 	// Load the config
-	$.getScript(CONFIG_PATHS["config"] + "global.js", function() {
+	// $.getScript(CONFIG_PATHS["config"] + "global.js", function() {
+	$.getScript(CONFIG_PATHS["relativeConfig"] + "global.js", function() {
 		// When the config is loaded, changePage
 		changePage("login", function(){
 			// When  the login is loaded,
@@ -14,7 +18,6 @@ $.getScript("resources/logic/config/paths.js", function() {
 	});
 	
 	$.getScript(CONFIG_PATHS["config"] + "default-server.js");
-	
 });
 
 // Load #top-bar and #content from filename
@@ -29,13 +32,17 @@ function changePage(fileName, callBack) {
 		}
 	}
 	
-	$.getScript(CONFIG_PATHS["core"] + fileName + ".js", onLoadFinished);
+	// $.getScript(CONFIG_PATHS["core"] + fileName + ".js", onLoadFinished);
+	$.getScript(CONFIG_PATHS["relativeCore"] + fileName + ".js", onLoadFinished);
 	// Clear page
 	$("#top-bar").html("");
 	$("#content").html("");
 	// Add the style
-	$("head").append('<link rel="stylesheet" href="'+CONFIG_PATHS["styles"] + fileName +'.css" type="text/css" />');
-	$("#top-bar").load(CONFIG_PATHS["pages"] + fileName + CONFIG_GLOBAL["htmlExtension"] + " #page-header", onLoadFinished);
-	$("#content").load(CONFIG_PATHS["pages"] + fileName + CONFIG_GLOBAL["htmlExtension"] + " #page-content", onLoadFinished);
+	// $("head").append('<link rel="stylesheet" href="'+CONFIG_PATHS["styles"] + fileName +'.css" type="text/css" />');
+	$("head").append('<link rel="stylesheet" href="'+CONFIG_PATHS["relativeStyles"] + fileName +'.css" type="text/css" />');
+	// $("#top-bar").load(CONFIG_PATHS["pages"] + fileName + CONFIG_GLOBAL["htmlExtension"] + " #page-header", onLoadFinished);
+	$("#top-bar").load(CONFIG_PATHS["relativePages"] + fileName + CONFIG_GLOBAL["htmlExtension"] + " #page-header", onLoadFinished);
+	//$("#content").load(CONFIG_PATHS["pages"] + fileName + CONFIG_GLOBAL["htmlExtension"] + " #page-content", onLoadFinished);
+	$("#content").load(CONFIG_PATHS["relativePages"] + fileName + CONFIG_GLOBAL["htmlExtension"] + " #page-content", onLoadFinished);
 }
 
